7d9aa461cc3f52078e7832291f7998ecbcf4217f
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-15 23:31:42 -0300

    chore: making the drf-spetacular settings in setting.py and adding the routes to generate swagger.io

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -30,6 +30,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External
+    'drf_spectacular',
+    # Internal
+    'account',
+    'server'
 ]
 
 MIDDLEWARE = [
@@ -114,3 +119,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.Account'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+}
