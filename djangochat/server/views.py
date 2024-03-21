7c99d6ea3802e5f70114233b8e213c4414447bea
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-21 00:54:26 -0300

    feat: Creating Filtering Servers and Returning Annotation

--- djangochat/server/views.py
+++ djangochat/server/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
+from django.db.models import Count
 
 from .serializer import ServerSerializer
 from .models import Server
@@ -15,6 +16,7 @@ class ServerListView(viewsets.ViewSet):
         quantity = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == 'true'
         by_serverid = request.query_params.get('by_serverid')
+        with_num_members = request.query_params.get('with_num_members') == 'true'
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -25,6 +27,9 @@ class ServerListView(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count('member'))
             
         if quantity:
             self.queryset = self.queryset[: int(quantity)]
@@ -37,5 +42,5 @@ class ServerListView(viewsets.ViewSet):
             except ValueError:
                 raise ValidationError(detail='Server value error')
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={'num_members': with_num_members})
         return Response(serializer.data)
