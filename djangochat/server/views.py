d1699c57438b6b4719c24ac8db2f33306c275eda
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-21 18:51:28 -0300

    feat: adding the server_list_docs decorator and doing a little refactoring

--- djangochat/server/views.py
+++ djangochat/server/views.py
@@ -3,14 +3,16 @@ from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 from django.db.models import Count
 
-from .serializer import ServerSerializer
 from .models import Server
+from .schema import server_list_docs
+from .serializer import ServerSerializer
 
 
 class ServerListView(viewsets.ViewSet):
 
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
         '''Return a List of servers filtered by various parameters.
 
@@ -48,28 +50,31 @@ class ServerListView(viewsets.ViewSet):
             GET /api/server/select/?by_user=true&qty=5
         '''
         category = request.query_params.get('category')
-        quantity = request.query_params.get('qty')
+        qty = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == 'true'
         by_serverid = request.query_params.get('by_serverid')
         with_num_members = request.query_params.get('with_num_members') == 'true'
-
-        if by_user or by_serverid and not request.user.is_authenticated:
-            raise AuthenticationFailed()
         
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if by_user and not request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
 
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count('member'))
             
-        if quantity:
-            self.queryset = self.queryset[: int(quantity)]
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
 
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+            
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
