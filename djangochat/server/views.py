97ac0e1fd3b5238a832e74465c6880bfcf6a7bbb
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-20 22:10:24 -0300

    feat: Creating an API Endpoint for filtering Severs by User

--- djangochat/server/views.py
+++ djangochat/server/views.py
@@ -13,12 +13,18 @@ class ServerListView(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get('category')
         quantity = request.query_params.get('qty')
+        by_user = request.query_params.get('by_user') == 'true'
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
+            
         if quantity:
             self.queryset = self.queryset[: int(quantity)]
+
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
