a10a03a921626ff2e5882aefc0181c594eaa35d3
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-21 19:34:59 -0300

    feat: Adding the icon attribute to the Category model and generating the migration

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDEIA_URL = 'media'
+MEDEIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
