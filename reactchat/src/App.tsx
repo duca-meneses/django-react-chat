0254df50f354af2c2230af74c0a1204b9906dfc2
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-21 21:47:06 -0300

    feat: Add router in App and exclude import unused

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,6 +1,16 @@
-function App() {
-  return <></>
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
 
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App
